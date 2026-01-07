d352a451a778cf9718d2bf7c5a833724c4b5bbf8
Thunder_Damo1606 <damodarsadavarte69@gmail.com>
2026-01-07 12:06:15 +0530

    Ignore flake8 E402 for test import

--- tests/test_training.py
+++ tests/test_training.py
@@ -4,7 +4,7 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(ROOT_DIR, "src"))
 
-from train import train_model
+from train import train_model  # noqa: E402
 
 
 def test_training_runs():
